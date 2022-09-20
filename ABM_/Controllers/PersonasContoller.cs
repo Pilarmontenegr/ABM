ebf5ffeef36517767d724aef1f3fe5b67c8e343f
Pilar Montenegro <nadiapilarm@gmail.com>
2022-09-20 12:09:02 -0300

    Agregamos vistas,Add,Change,Delete

--- ABM_/Controllers/PersonasContoller.cs
+++ ABM_/Controllers/PersonasContoller.cs
@@ -12,5 +12,20 @@ namespace ABM_.Controllers
             //le paso el nombre de la vista
             return View("Personas");
         }
+        public IActionResult Add()
+        {
+            //le paso el nombre de la vista
+            return View("Add");
+        }
+        public IActionResult Delete()
+        {
+            //le paso el nombre de la vista
+            return View("Delete");
+        }
+        public IActionResult Change()
+        {
+            //le paso el nombre de la vista
+            return View("Change");
+        }
     }
 }
