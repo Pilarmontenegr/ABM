316961c29040c5e79f5663137d02fb85f9e407dc
Fran Radicci <francosoff7@hotmail.com>
2022-09-21 11:18:52 -0300

    Comentamos el "if"

--- ABM_/Data/ApplicationDbContext.cs
+++ ABM_/Data/ApplicationDbContext.cs
@@ -19,11 +19,11 @@ namespace ABM_.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //conectamos nustra base SQL SERVER usando string de conexión
-            if (!optionsBuilder.IsConfigured)
-            {                                 //PONGO NOMBRE 
-                optionsBuilder.UseSqlServer("Data Source=PILI\\SQLEXPRESS;Initial Catalog=aspnet-ABM_-2799B4DD-A57F-4096-8EF6-70471F0DD644;Integrated Security=True;Connect" +
-                    " Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
+            //if (!optionsBuilder.IsConfigured)
+            //{                                 //PONGO NOMBRE 
+            //    optionsBuilder.UseSqlServer("Data Source=PILI\\SQLEXPRESS;Initial Catalog=aspnet-ABM_-2799B4DD-A57F-4096-8EF6-70471F0DD644;Integrated Security=True;Connect" +
+            //        " Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //}
 
         }
     }
