78bfbb56df5348e02bb43ff892016124a3647672
Pilar Montenegro <nadiapilarm@gmail.com>
2022-09-27 12:12:45 -0300

    Vista Peronas

--- ABM_/Controllers/PersonasController.cs
+++ ABM_/Controllers/PersonasController.cs
@@ -8,14 +8,14 @@ namespace ABM_.Controllers
 {
     public class PersonasController : Controller
     {
-        
-         private readonly ApplicationDbContext _context;
 
-         public PersonasController(ApplicationDbContext context)
-            {
+        private readonly ApplicationDbContext _context;
+
+        public PersonasController(ApplicationDbContext context)
+        {
             _context = context;
-            }
-        
+        }
+
 
         public IActionResult Index()
         {
@@ -23,26 +23,27 @@ namespace ABM_.Controllers
             //llamo al _contex
             var personas = _context.Personas.ToList();
             //le paso el nombre de la vista
-            return View("Personas",personas);
+            //habiamos puesto ,personas//
+            return View("Persona",personas);
         }
         public IActionResult Add()
         {
             //le paso el nombre de la vista
-            return View("Add");
+            return View();
         }
         public IActionResult Delete()
         {
             //le paso el nombre de la vista
-            return View("Delete");
+            return View();
         }
         public IActionResult Change()
         {
             //le paso el nombre de la vista
-            return View("Change");
+            return View();
         }
-        
-        
+
+
     }
 }
 
-   
+
