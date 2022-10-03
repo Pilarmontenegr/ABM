25454db579d254fd6bdaa6d6c15eafebe468b2b7
Pilar Montenegro <nadiapilarm@gmail.com>
2022-10-03 10:37:20 -0300

    Subo vista "Add"

--- ABM_/Controllers/PersonasController.cs
+++ ABM_/Controllers/PersonasController.cs
@@ -3,6 +3,7 @@ using ABM_.Data;
 using ABM_.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ABM_.Controllers
 {
@@ -26,21 +27,59 @@ namespace ABM_.Controllers
             //habiamos puesto ,personas//
             return View("Persona",personas);
         }
+        
+        
         public IActionResult Add()
+        
         {
             //le paso el nombre de la vista
-            return View();
+            return View("Add");
         }
+        //POST
+        [HttpPost] //este metodo agarra la acción POST
+        public async Task<IActionResult> Create(Persona persona)
+        {
+            try //para manejar errores y excepciones
+            {    
+                //para agregar los datos recibidos
+                _context.Add(persona);
+                await _context.SaveChangesAsync();
+                                               //Retorna los nuevos datos a la vista que muestra las personas
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+
+
         public IActionResult Delete()
         {
             //le paso el nombre de la vista
             return View();
         }
-        public IActionResult Change()
-        {
-            //le paso el nombre de la vista
-            return View();
-        }
+        //public IActionResult Edit(int id)
+        //{   //preguntamos si existe el id 
+        //    if (_context.Personas.Any(x => x.Id == id))
+        //    {    //si existe lo recuperamos de la bd
+        //        Persona modelo = _context.Personas.Select(p => new Persona
+        //        {
+        //            Id = p.Id,
+        //            Nombre = p.Nombre,
+        //            Apellido = p.Apellido,
+        //            Dni = p.Dni,
+        //            Domicilio = p.Domicilio,
+        //            Edad = p.Edad
+        //        }).First(x => x.Id == id);
+
+        //        return View(modelo);
+        //    }
+            //lo redireccionamos al axion error del controller home
+            //return RedirectToAction("Error", "Home");
+        //}
 
 
     }
--- ABM_/Models/PersonasViewModels.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ABM_.Models
-{
-    public class PersonasViewModels
-    {              //hace referencia a la clase persona
-        public List<Persona>? Personas { get; set; }
-    }
-}
